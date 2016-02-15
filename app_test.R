0c75486c7c5ce8d094aa153c51b3586cb91af694
collinschwantes <cjschwantesll@gmail.com>
2016-02-15 12:09:03 -0500

    Initial Commit
    This project will be used to build interactive annotated images of high
    resolution images.

---
+++ app_test.R
@@ -0,0 +1,6 @@
+library(shiny)
+library(svgPanZoom)
+library(Cairo)
+library(gridSVG)
+
+
---
+++ demoweb/script.js
@@ -0,0 +1,18 @@
+//document.getElementById("foot01").innerHTML =
+//"<p>&copy;  " + new Date().getFullYear() + " Smithsonian Institute All //rights reserved.</p>";
+
+
+$(function(){
+
+
+var waspPanZoom = svgPanZoom('#Layer_1');
+
+var waspsvg = $(".panzoom svg");
+
+console.log(waspsvg);
+
+//document.getElementById('#Layer_1').querySelector('rect').setAttribute('width', 1024);//
+
+});
+
+
