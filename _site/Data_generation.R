7ba4438d9682ce0c2ff60890f82d2c1df93460db
Brian Hedberg <bshslowfood@gmail.com>
2016-02-21 12:01:59 -0600

    remove _site

--- _site/Data_generation.R
+++
@@ -1,5 +0,0 @@
-setwd("/Users/featherlite569/Documents/NMNH/wasp_app/demoweb")
-hao_terms <- read.csv(file = "./HAO_terms.csv")
-hao_terms <- hao_terms[!duplicated(hao_terms[,1]),]
-hao_terms[,1] <- capitalize(hao_terms[,1])
-write(toJSON(hao_terms),file = "hao_terms.json")
