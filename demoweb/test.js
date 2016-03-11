809d34c541d9e31a0404c83fe7a1a3fd893a8bf0
collinschwantes <cjschwantesll@gmail.com>
2016-03-11 11:21:03 -0500

    Trashed files
    trashed junk files

--- demoweb/test.js
+++
@@ -1,8 +0,0 @@
-function reqListener () {
-  console.log(this.responseText);
-}
-
-var oReq = new XMLHttpRequest();
-oReq.addEventListener("load", reqListener);
-oReq.open("GET", "http://purl.obolibrary.org/obo/HAO_0000397");
-oReq.send();
